685defeaf7b025035bf03483015fd4d951830996
malave312 <malaveu@gmail.com>
2022-11-03 17:28:30 -0400

    Update

--- pagina-youtube/js/index.js
+++ pagina-youtube/js/index.js
@@ -9,9 +9,7 @@ const inputSearch = document.getElementById("input-search");
 const btnClear = document.getElementById("clear");
 const form = document.querySelector("form");
 
-
-const
-    // card = document.getElementById('container-card'),
+const // card = document.getElementById('container-card'),
     cardHover = document.getElementsByClassName("section2");
 
 const btns = document.getElementsByClassName("link"),
@@ -47,8 +45,6 @@ inputSearch.addEventListener("focusin", function (e) {
 });
 
 inputSearch.addEventListener("focusout", function (e) {
-    // document.querySelector(".navbar-section2-container-action").style.outline =
-    // "0";
     document
         .querySelector(".navbar-section2-container-action-search")
         .classList.remove("style-input");
@@ -97,26 +93,66 @@ const getVideo = (resultVideo) => {
     section2.style.gridTemplateRows = `repeat(${
         resultVideo.length / 4
     }, 286px)`;
-    const cardHover = document.querySelectorAll('.container-card');
-    for (let b = 0; b < cardHover.length; b++) {
-        cardHover[b].addEventListener('mouseover', function (e) {
-            if (e.target.classList.contains("image") == true) {
-                cardHover[b].classList.add("container-card-h");
-                // console.log(cardHover[b]);
-                cardHover[b].querySelector(".container-ob").classList.add("container-ob-h");
-                cardHover[b].querySelector(".title").classList.add("title-h");
-                cardHover[b].querySelector(".user").classList.add("user-h");
-                cardHover[b].querySelector(".views").classList.add("views-h");
-            }
-            // if (e.target.classList.contains("image") == false || e.target.classList.contains("container-card") == true) {
-            //     cardHover[b].classList.remove("container-card-h");
-            // }
-        });
-        // cardHover[b].addEventListener('mouseout', function (e) {
-        //     if (e.target.classList.contains("image") == false) {
-        //     }
-        // console.log(cardHover[b]);
+    const cardHover = document.querySelectorAll(".container-card");
+    videoThumbnail(cardHover);
+};
+
+const videoThumbnail = (cardHover) => {
+
+    for (let i = 0; i < cardHover.length; i++) {
+        // window.addEventListener('load', function (e) {
+        //     cardHover[i].querySelector(".image").onmouseover = standBy;
+        //     cardHover[i].onmouseout = cancel;
         // });
+        cardHover[i].addEventListener('mouseenter', function (e) {
+            cardHover[i].querySelector(".image").onmouseover = standBy;
+            cardHover[i].onmouseout = cancel;
+        });
+
+        function standBy() {
+            if (cardHover[i].dataset.state == "true" && cardHover[i].classList.contains("card-hv")) {
+                return
+            }
+            standBy.time = setTimeout(showCards, 1000);
+            for (let i = 0; i < cardHover.length; i++) {
+                if (cardHover[i].dataset.state == "true") {
+                    cardHover[i].classList.remove("card-hv");
+                    cardHover[i].querySelector('.title').classList.add("tl-hv");
+                cardHover[i].querySelector('.user').classList.add("usr-hv");
+                cardHover[i].querySelector('.views').classList.add("vws-hv");
+                }
+            }
+            // console.log(document.querySelector('.section1').classList.contains("container-card"));
+
+        }
+
+        function cancel() {
+            // console.log(document.querySelector('.section1').classList.contains("container-card"));
+            if (cardHover[i].dataset.state == "true") {
+                return
+            }
+            for (let i = 0; i < cardHover.length; i++) {
+                if (cardHover[i].dataset.state == "true") {
+                    cardHover[i].classList.remove("card-hv");
+                    cardHover[i].querySelector('.title').classList.add("tl-hv");
+                cardHover[i].querySelector('.user').classList.add("usr-hv");
+                cardHover[i].querySelector('.views').classList.add("vws-hv");
+                    cardHover[i].dataset.state = false;
+                }
+            }
+            clearTimeout(standBy.time);
+        }
+
+        function showCards(e) {
+            if (cardHover[i].dataset.state == "false") {
+                cardHover[i].classList.add("card-hv");
+                // cardHover[i].classList.add("fade");
+                cardHover[i].querySelector('.title').classList.add("tl-hv");
+                cardHover[i].querySelector('.user').classList.add("usr-hv");
+                cardHover[i].querySelector('.views').classList.add("vws-hv");
+                cardHover[i].dataset.state = true;
+            }
+        }
     }
 };
 
