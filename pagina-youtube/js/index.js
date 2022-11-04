cce2e922f69fae936294b956f14dea5f8f86d1d9
malave312 <malaveu@gmail.com>
2022-11-04 17:35:11 -0400

    prueba

--- pagina-youtube/js/index.js
+++ pagina-youtube/js/index.js
@@ -2,8 +2,8 @@ const containerVideo = document.getElementById("container-video"),
     template = document.getElementById("template").content,
     fragment = document.createDocumentFragment(),
     cards = document.getElementById("container-card"),
-    section2 = document.querySelector(".main-container > .section2");
-// cardsHover = document.querySelectorAll(".container-card");
+    section2 = document.querySelector(".main-container > .section2"),
+    iconBar = document.getElementById('icon-nav');
 
 const inputSearch = document.getElementById("input-search");
 const btnClear = document.getElementById("clear");
@@ -61,6 +61,17 @@ inputSearch.addEventListener("focusout", function (e) {
     // document.querySelector('.bi-search').classList.replace('invisible', 'visible');
 });
 
+iconBar.addEventListener('click', function (e) {
+    console.log("click");
+    // document.querySelector('.section1').style.width = "85px";
+    // document.querySelector('.section1').classList.toggle("wd");
+    document.querySelector('.section1').classList.toggle("toggle-nav");
+    document.querySelector('.bar-section3').classList.toggle("d-none");
+    document.querySelector('.clock').classList.toggle("d-none");
+    // document.querySelector('.active').classList.add("bg-white");
+
+});
+
 const callApi = async () => {
     try {
         const apiKey = "23543280-ab1316dfc9ab1ef53517801e4";
@@ -76,12 +87,13 @@ const callApi = async () => {
 
 const getVideo = (resultVideo) => {
     for (let i = 0; i < resultVideo.length; i++) {
-        const { largeImageURL, userImageURL, user, views, published } =
+        const { largeImageURL, userImageURL, user, views, webformatURL,pageURL } =
             resultVideo[i];
         const clone = template.cloneNode(true);
-        clone.querySelector(".image").setAttribute("src", `${largeImageURL}`);
+        clone.querySelector(".image").setAttribute("src",  webformatURL);
         clone.querySelector(".image").setAttribute("alt", `prueba${i}`);
-        clone.querySelector(".user-img").setAttribute("src", `${userImageURL}`);
+        clone.querySelector(".user-img").setAttribute("src", userImageURL);
+        clone.querySelector(".href").setAttribute("href", pageURL);
         clone.querySelector(".user-img").setAttribute("alt", `prueba${i}`);
         clone.querySelector(".user").textContent = user;
         clone.querySelector(
@@ -92,23 +104,21 @@ const getVideo = (resultVideo) => {
     cards.appendChild(fragment);
     section2.style.gridTemplateRows = `repeat(${
         resultVideo.length / 4
-    }, 286px)`;
+    }, 306px)`;
     const cardHover = document.querySelectorAll(".container-card");
     videoThumbnail(cardHover);
 };
 
 const videoThumbnail = (cardHover) => {
-
     for (let i = 0; i < cardHover.length; i++) {
-        // window.addEventListener('load', function (e) {
-        //     cardHover[i].querySelector(".image").onmouseover = standBy;
-        //     cardHover[i].onmouseout = cancel;
-        // });
-        cardHover[i].addEventListener('mouseenter', function (e) {
+        window.addEventListener('load', function (e) {
             cardHover[i].querySelector(".image").onmouseover = standBy;
             cardHover[i].onmouseout = cancel;
         });
-
+        // cardHover[i].addEventListener('mouseenter', function (e) {
+        //     cardHover[i].querySelector(".image").onmouseover = standBy;
+        //     cardHover[i].onmouseout = cancel;
+        // });
         function standBy() {
             if (cardHover[i].dataset.state == "true" && cardHover[i].classList.contains("card-hv")) {
                 return
@@ -118,11 +128,10 @@ const videoThumbnail = (cardHover) => {
                 if (cardHover[i].dataset.state == "true") {
                     cardHover[i].classList.remove("card-hv");
                     cardHover[i].querySelector('.title').classList.add("tl-hv");
-                cardHover[i].querySelector('.user').classList.add("usr-hv");
-                cardHover[i].querySelector('.views').classList.add("vws-hv");
+                    cardHover[i].querySelector('.user').classList.add("usr-hv");
+                    cardHover[i].querySelector('.views').classList.add("vws-hv");
                 }
             }
-            // console.log(document.querySelector('.section1').classList.contains("container-card"));
 
         }
 
